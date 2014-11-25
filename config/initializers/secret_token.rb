396c4fa3abc02306c02c59d3b6fd361b39f6be28
BobcatMK <matthew.kilan@gmail.com>
2014-11-25 15:22:43 +0100

    First push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakeFullRails::Application.config.secret_key_base = '83aa09e88b460e8aa2494864c7934e90e720ae13d4a705b32da35b1429de93ce2b07cbb7d47eeab958eb428623cbb5684a4f8a44ffdc46c51c1492b8766f2a6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FakeFullRails::Application.config.session_store :cookie_store, key: '_fake_full_rails_session'
